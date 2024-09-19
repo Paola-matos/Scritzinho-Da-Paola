8dd7126184b844fbf818f05b0c4b9c58dfd9751f
Paola-matos <162580325+Paola-matos@users.noreply.github.com>
2024-09-19 08:42:45 -0300

    Add files via upload

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,19 @@
+
+console.log("trabalhando com variaveis");
+const nome = "Ricardo";
+const sobrenome = "Bugan";
+
+//console.log(nome + " " + sobrenome)
+console.log(nome, sobrenome)
+console.log('Meu nome é ${nome}');
+
+let contador = 0
+contador = contador +1
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade = 26; //atribuindo valor 
+idade = idade+1;
+console.log(idade);
